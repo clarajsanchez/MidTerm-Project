0b315be57e1002f086c0b04521c5c38b427521bf
clarajsanchez <clarajsanchez@hotmail.com>
2022-11-24 10:08:41 +0100

    upload project page cambia depende d los botones

--- project/proj.js
+++ project/proj.js
@@ -5,10 +5,12 @@ if (projNum === 2) {
     document.getElementById("project-subtitulo").innerHTML = "Web Development";
     document.getElementsByTagName("img")[0].src = "/assets/projects-section/2.jpg";
     document.getElementsByTagName("img")[1].src = "/assets/projects-section/2.jpg";
+    document.getElementById("proj-texto").innerHTML = "Dashcoin-text";
 }
 else if (projNum === 3) {
     document.getElementById("project-titulo").innerHTML = "Vectorify";
     document.getElementById("project-subtitulo").innerHTML = "User Experience Design";
     document.getElementsByTagName("img")[0].src = "/assets/projects-section/3.jpg";
     document.getElementsByTagName("img")[1].src = "/assets/projects-section/3.jpg";
+    document.getElementById("proj-texto").innerHTML = "Vectorify-text";
 }
--- projectSection/projectSection.js
+++
@@ -1,5 +0,0 @@
-
-// document.getElementById("simplify").onclick = function () {
-//     console.log("cambio");
-//     document.getElementById("titulo").innerHTML = "<h2>Other Projects</h2>";
-// }
