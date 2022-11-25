429a9839c9e26637322bbc715891a209e20f3a5c
clarajsanchez <clarajsanchez@hotmail.com>
2022-11-25 10:11:51 +0100

    burger menu worksgit add .!

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -1,18 +1,4 @@
-function repliegue() {
-  let isShown = document.getElementsByClassName("display-menu");
-  let isHidden = document.getElementsByClassName("hide-menu");
-  console.log(isShown);
-  console.log(isHidden);
-  if (isHidden !== null) {
-    console.log("isHidden");
-    $("#main-menu-column").removeClass("hide-menu");
-    $("#main-menu-column").addClass("display-menu");
-    isHidden = null;
-  } else if (isShown !== null) {
-    console.log("isShown");
-    $("#main-menu-column").removeClass("display-menu");
-    $("#main-menu-column").addClass("hide-menu");
-    console.log("he llegado");
-    isShown = null;
-  }
+function burgerMenu() {
+  document.querySelector("#main-menu-column").classList.toggle("display-menu");
+  document.querySelector("#nav").classList.toggle("padding-nav");
 }
